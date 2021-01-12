e47f3685eb54c2706e270e8ceb641834c61fdfae
Diman4a12 <73233078+Diman4a12@users.noreply.github.com>
2021-01-12 22:26:16 +0500

    1

--- MultiFilter1.py
+++ MultiFilter1.py
@@ -8,9 +8,17 @@ def mul5(x):
     return x % 5 == 0
 
 
-a = [i for i in range(31)]
+a = [i for i in range(31)] # [0, 1, 2, ... , 30]
+
+
 
 class multifilter:
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        self.iterable = iterable
+        
+        # iterable - исходная последовательность
+        # funcs - допускающие функции
+        # judge - решающая функция
     def judge_half(pos, neg):
         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
 
@@ -20,10 +28,7 @@ class multifilter:
     def judge_all(pos, neg):
         # допускает элемент, если его допускают все функции (neg == 0)
 
-    def __init__(self, iterable, *funcs, judge=judge_any):
-        # iterable - исходная последовательность
-        # funcs - допускающие функции
-        # judge - решающая функция
+    
 
     def __iter__(self):
         # возвращает итератор по результирующей последовательности
